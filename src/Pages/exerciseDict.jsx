38b99853b6de05cb84012bf8d16ca5bf111a01d6
JacobiGlenn <glennjg@uci.edu>
2025-11-05 22:46:59 -0800

    Updated workout dict

--- src/Pages/exerciseDict.jsx
+++ src/Pages/exerciseDict.jsx
@@ -2,7 +2,6 @@
 import inclinedumbellpress from '../workoutPhotos/inclinedumbellpress.webp';
 
 export const exerciseDict = {
-<<<<<<< HEAD
     format: ["exercise name", "image source", "how to do workout (video link)", "equipment needed", "muscle group"],
 
     chest: {
@@ -65,12 +64,4 @@ export const exerciseDict = {
         cardio4: ["Jump Rope", "picture", "how to do workout", "jump rope", "cardio"],
         cardio5: ["Burpees", "picture", "how to do workout", "bodyweight", "cardio and full body"],
     }
-=======
-    chest: ["chest1", "chest2"],
-    arm: [],
-    leg: [],
-    back: [],
-    abs: [],
-    cardio: []
->>>>>>> 1734667 (did cards and stuff)
 }
