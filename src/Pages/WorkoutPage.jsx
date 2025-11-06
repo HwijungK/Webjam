dc0b79a412f48d6ef8df9a2833699136148c7aac
Hwijung Kim <hwi.meh@gmail.com>
2025-11-05 17:15:45 -0800

    empty dict

--- src/Pages/WorkoutPage.jsx
+++ src/Pages/WorkoutPage.jsx
@@ -4,19 +4,19 @@ export default function WorkoutPage() {
     return (
         <>
             <h1>Workout</h1>
-            <WorkoutCard exercise="pullUps" reps="7"></WorkoutCard>
-            <WorkoutCard exercise="chestPress" reps="8"></WorkoutCard>
+            <WorkoutCard exercise="pullUps" target="back" reps="7"></WorkoutCard>
+            <WorkoutCard exercise="chestPress" target="chest" reps="8"></WorkoutCard>
         </>
     )
 }
 
-
-function WorkoutCard({exercise, reps}) {
+// abs, arms, legs, chest, back, cardio
+function WorkoutCard({exercise, target, reps}) {
 
     return(
         <div className="workout-card">
             <h2>{exercise}</h2>
-            <h3>Target: {exerciseDict[exercise].target}</h3>
+            <h3>Target: {target}</h3>
             <h3>3 sets; {reps} reps</h3>
         </div>
     )
