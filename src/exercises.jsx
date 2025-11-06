58d8deefde8a5a366f9caf159400cdb988b355ab
Abhinav Kumar <74889782+AbhinavKumar07@users.noreply.github.com>
2025-11-06 01:18:35 +0000

    updated bmi and workout calculations

--- src/exercises.jsx
+++ src/exercises.jsx
@@ -1,3 +1,28 @@
-function getWorkout(height, weight, gender, days) {
-    
+function getWorkout(height, weight, days) {
+  //weight (lb) / h^2 (in)
+  // if below 16
+  //elif 17-30 medium
+  //else height
+
+  baseReps = 10; //reps per set
+  baseSets = 3; // # of total sets
+  baseCardio = 0;
+
+  obesityLevel = "";
+
+  bmi = weight / height ** 2;
+
+  if (bmi <= 16) {
+    obesityLevel = "LOW";
+    baseReps -= 3;
+    baseSets -= 1;
+  } else if (bmi > 16 && bmi < 30) {
+    obesityLevel = "MEDIUM";
+  } else {
+    obesityLevel = "HIGH";
+    baseSets += 3;
+    baseCardio += 1;
+
+    //{target : [exercises]} -- exercise list
+  }
 }
