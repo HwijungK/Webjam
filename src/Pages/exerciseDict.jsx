e698e185defa74842110eef0ce9e7545cb2c31a6
Hwijung Kim <hwi.meh@gmail.com>
2025-11-06 11:21:17 -0800

    removed duplicate entries

--- src/Pages/exerciseDict.jsx
+++ src/Pages/exerciseDict.jsx
@@ -1,11 +1,15 @@
 //picture imports
 import inclinedumbellpress from '../workoutPhotos/inclinedumbellpress.webp';
 
+//picture imports
+import inclinedumbellpress from '../workoutPhotos/inclinedumbellpress.webp';
+
 export const exerciseDict = {
     format: ["exercise name", "image source", "how to do workout (video link)", "equipment needed", "muscle group"],
 
+    format: ["exercise name", "image source", "how to do workout (video link)", "equipment needed", "muscle group"],
+
     chest: {
-<<<<<<< HEAD
         chest1: ["Incline Dumbell Chest Press", inclinedumbellpress, "https://www.youtube.com/shorts/8fXfwG4ftaQ", "dumbells", "upper chest"],
         chest2: ["Decline Dumbell Chest Press", "picture", "how to do workout", "dumbells", "lower chest"],
         chest3: ["Dumbell Chest Fly", "picture", "how to do workout", "dumbells", "chest"],
@@ -16,15 +20,6 @@ export const exerciseDict = {
         chest8: ["Decline Push Ups", "picture", "how to do workout", "bodyweight", "lower chest"],
         chest9: ["Dumbell Pullover", "picture", "how to do workout", "dumbell", "chest and lats"]
     },
-=======
-        chest1: ["Incline Dumbell Chest Press", "picture", "how to do workout", "dumbells"],
-        chest2: ["Decline Dumbell Chest Press", "picture", "how to do workout", "dumbells"],
-        chest3: ["Dumbell Chest Fly", "picture", "how to do workout", "dumbells"],
-        chest4: ["Push Ups", "picture", "how to do workout", "bodyweight"], 
-        chest5: ["Cable Chest Fly", "picture", "how to do workout", "cable machine"],
-        chest6: ["Diamond Push Ups", "picture", "how to do workout", "bodyweight"]
-},
->>>>>>> 10d04cc (chest pictures added)
     arm: {
         arm1: ["Bicep Curls", "picture", "how to do workout", "dumbells", "biceps"],
         arm2: ["Tricep Dips", "picture", "how to do workout", "bodyweight", "triceps"],
@@ -38,34 +33,34 @@ export const exerciseDict = {
         arm10: ["Cable Tricep Pushdowns", "picture", "how to do workout", "cable machine", "triceps"]
     },
     leg: {
-        leg1: ["Squats", "picture", "how to do workout", "bodyweight or dumbells", "quadriceps, hamstrings, glutes"],
-        leg2: ["Lunges", "picture", "how to do workout", "bodyweight or dumbells", "quadriceps"],
-        leg3: ["Leg Press", "picture", "how to do workout", "leg press machine", "quadriceps, hamstrings, glutes"],
-        leg4: ["Calf Raises", "picture", "how to do workout", "bodyweight or dumbells", "calves"],
-        leg5: ["Leg Curls", "picture", "how to do workout", "leg curl machine", "hamstrings"],
-        leg6: ["Laying Leg Curl", "picture", "how to do workout", "leg extension machine", "hamstrings"],
-        leg7: ["Step Ups", "picture", "how to do workout", "bench or box", "quadriceps, hamstrings, glutes"],
-        leg8: ["Glute Bridges", "picture", "how to do workout", "bodyweight or dumbells", "glutes"],
-        leg9: ["Bulgarian Split Squats", "picture", "how to do workout", "bodyweight or dumbells", "quadriceps, hamstrings, glutes"],
-        leg10: ["Goblet Squats", "picture", "how to do workout", "dumbell or kettlebell", "quadriceps, hamstrings, glutes"]
+        leg1: ["Squats", "picture", "how to do workout", "bodyweight or dumbells", "quadriceps, hamstrings, glutes", "quadriceps, hamstrings, glutes"],
+        leg2: ["Lunges", "picture", "how to do workout", "bodyweight or dumbells", "quadriceps", "quadriceps"],
+        leg3: ["Leg Press", "picture", "how to do workout", "leg press machine", "quadriceps, hamstrings, glutes", "quadriceps, hamstrings, glutes"],
+        leg4: ["Calf Raises", "picture", "how to do workout", "bodyweight or dumbells", "calves", "calves"],
+        leg5: ["Leg Curls", "picture", "how to do workout", "leg curl machine", "hamstrings", "hamstrings"],
+        leg6: ["Laying Leg Curl", "picture", "how to do workout", "leg extension machine", "hamstrings", "hamstrings"],
+        leg7: ["Step Ups", "picture", "how to do workout", "bench or box", "quadriceps, hamstrings, glutes", "quadriceps, hamstrings, glutes"],
+        leg8: ["Glute Bridges", "picture", "how to do workout", "bodyweight or dumbells", "glutes", "glutes"],
+        leg9: ["Bulgarian Split Squats", "picture", "how to do workout", "bodyweight or dumbells", "quadriceps, hamstrings, glutes", "quadriceps, hamstrings, glutes"],
+        leg10: ["Goblet Squats", "picture", "how to do workout", "dumbell or kettlebell", "quadriceps, hamstrings, glutes", "quadriceps, hamstrings, glutes"]
     },
     back: {
-        back1: ["Pull Ups", "picture", "how to do workout", "pull up bar", "back and biceps"],
-        back2: ["Bent Over Rows", "picture", "how to do workout", "dumbells or barbell on bench", "upper back"],
-        back3: ["Lat Pulldowns", "picture", "how to do workout", "lat pulldown machine", "lats"],
-        back4: ["Seated Cable Rows", "picture", "how to do workout", "cable machine", "middle back"],
-        back5: ["Deadlifts", "picture", "how to do workout", "barbell or dumbells", "lower back, glutes, hamstrings"],
-        back6: ["Single Arm Dumbell Rows", "picture", "how to do workout", "dumbell and bench", "upper back"],
-        back7: ["Face Pulls", "picture", "how to do workout", "cable machine", "rear delts and upper back"],
+        back1: ["Pull Ups", "picture", "how to do workout", "pull up bar", "back and biceps", "back and biceps"],
+        back2: ["Bent Over Rows", "picture", "how to do workout", "dumbells or barbell on bench", "upper back", "upper back"],
+        back3: ["Lat Pulldowns", "picture", "how to do workout", "lat pulldown machine", "lats", "lats"],
+        back4: ["Seated Cable Rows", "picture", "how to do workout", "cable machine", "middle back", "middle back"],
+        back5: ["Deadlifts", "picture", "how to do workout", "barbell or dumbells", "lower back, glutes, hamstrings", "lower back, glutes, hamstrings"],
+        back6: ["Single Arm Dumbell Rows", "picture", "how to do workout", "dumbell and bench", "upper back", "upper back"],
+        back7: ["Face Pulls", "picture", "how to do workout", "cable machine", "rear delts and upper back", "rear delts and upper back"],
     },
     abs: {
-        abs1: ["Plank", "picture", "how to do workout", "bodyweight", "core"],
-        abs2: ["Russian Twists", "picture", "how to do workout", "bodyweight or dumbell", "obliques"],
-        abs3: ["Leg Raises", "picture", "how to do workout", "bodyweight", "lower abs"],
-        abs4: ["Bicycle Crunches", "picture", "how to do workout", "bodyweight", "abs"],
-        abs5: ["Mountain Climbers", "picture", "how to do workout", "bodyweight", "core and cardio"],
-        abs6: ["Sit Ups", "picture", "how to do workout", "bodyweight", "abs"],
-        abs7: ["Hanging Leg Raises", "picture", "how to do workout", "pull up bar", "lower abs"],
+        abs1: ["Plank", "picture", "how to do workout", "bodyweight", "core", "core"],
+        abs2: ["Russian Twists", "picture", "how to do workout", "bodyweight or dumbell", "obliques", "obliques"],
+        abs3: ["Leg Raises", "picture", "how to do workout", "bodyweight", "lower abs", "lower abs"],
+        abs4: ["Bicycle Crunches", "picture", "how to do workout", "bodyweight", "abs", "abs"],
+        abs5: ["Mountain Climbers", "picture", "how to do workout", "bodyweight", "core and cardio", "core and cardio"],
+        abs6: ["Sit Ups", "picture", "how to do workout", "bodyweight", "abs", "abs"],
+        abs7: ["Hanging Leg Raises", "picture", "how to do workout", "pull up bar", "lower abs", "lower abs"],
     },
     cardio: {
         cardio1: ["Running", "picture", "how to do workout", "treadmill or outdoor", "cardio"],
